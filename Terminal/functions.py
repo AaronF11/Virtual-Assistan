182180f84ce7455112b8ab3779f149232b4e1653
Aaron Flores Pasos <aaron.flores7908@alumnos.udg.mx>
2021-12-10 23:29:44 -0600

    Version ingles y español

--- Terminal/functions.py
+++ Terminal/functions.py
@@ -1,25 +1,78 @@
 # --------------- headers --------------- #
 from utils import *
-import speech_recognition as SR
 
-# --------------- Functions --------------- #
-def engine_of_voice(text):
+
+# --------------- Functions engine_of_voice --------------- #
+# Voices
+    # Spanish-Mexican[0]
+    # English-EUA[1]
+
+def engine_of_voice_sp(text):
+    
+    engine.setProperty('voice', voices[0].id)       
     engine.say(text)
     engine.runAndWait()
 
-def listening():
-    listener = SR.Recognizer()
+def engine_of_voice_en(text):
+    engine.setProperty('voice', voices[1].id)       
+    engine.say(text)
+    engine.runAndWait()
+
+
+# --------------- Function listening --------------- #
+def listen():
     try:
         with SR.Microphone() as source:
-            print("LISTENING...")
+            print(""" - - - SELECT A LENGUAGE - - - 
+            + SPANISH
+            + ENGLISH""")
             voice = listener.listen(source)
-            rec = listener.recognize_google(voice)
-            print(rec)
-            engine_of_voice(rec)
-    except :
-        print("REPEAT PLEAS")
+            recognizer = listener.recognize_google(voice)
+            recognizer = recognizer.lower()
+
+            if 'spanish' in recognizer:
+                try:
+                    with SR.Microphone() as source:
+                        print(listening[0])
+                        voice = listener.listen(source)
+                        recognizer = listener.recognize_google(voice)
+                        recognizer = recognizer.lower()
+                        if 'alexa' in recognizer:
+                            engine_of_voice_sp(recognizer)    
+                except:
+                    print(repeat[0])
+
+                return recognizer
+
+            if 'english' in recognizer:
+                try:
+                    with SR.Microphone() as source:
+                        print(listening[1])
+                        voice = listener.listen(source)
+                        recognizer = listener.recognize_google(voice)
+                        recognizer = recognizer.lower()
+                        if 'alexa' in recognizer:
+                            engine_of_voice_en(recognizer)    
+                except:
+                    print(repeat[1])
+
+                return recognizer
+    except:
+        pass
+        
+    
+# --------------- Function action --------------- #
+def action():
+    recognizer = listen()
+
+    if 'reproduce' in recognizer:
+        print(playing[0])
+    
+    if 'play' in recognizer:
+        print(playing[1])
+
 
 # --------------- main --------------- #
 def main():
-    listening()
+    action()
     
